080ad0c9b6b5f17f740412f5266ed9a738230022
Dukeson Ehigboria O <studysampul@gmail.com>
2023-05-09 13:17:58 +0100

    Updated my work

--- main.py
+++ main.py
@@ -11,9 +11,22 @@ def accept_val(prompt: str) -> int:
 
 if __name__ == "__main__":
     import math_func as mth
+    try:
+        first = accept_val("First Value")
+        second = accept_val("Second Value")
 
-    first = accept_val("First Value")
-    second = accept_val("Second Value")
+        add_result = mth.addition(first, second)
+        print(f"The sum of {first} and {second} = {add_result}")
+    
+        sub_result = mth.subtract(first, second)
+        print(f"The difference between {first} and {second} = {sub_result}")
 
-    result = mth.add(first, second)
-    print(f"The sum of {first} and {second} = {result}")    
+        mul_result = mth.multiply(first, second)
+        print(f"The product of {first} and {second} = {mul_result}")
+
+        div_result = mth.divide(first, second)
+        print(f"{first} divided bt {second} = {div_result}")
+
+        print(f"12 divided by 0 = {mth.divide(12, 0)}")
+    except (ValueError, TypeError, ZeroDivisionError) as err:
+        print(err)
--- math_func.py
+++ math_func.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python3
 
-def add(val_a: int|float, val_b: int|float) -> int | float:
+def addition(val_a: int|float, val_b: int|float) -> int | float:
     if type(val_a) == int or type(val_a) == float:
         if type(val_b) == float or type(val_b) == int:
             return val_a + val_b
+        else:
+            raise Exception("Second value must be numeric.")
+    else:
+        raise Exception("First value must be numeric.")
+
+def subtract(val_a: int|float, val_b: int|float) -> int | float:
+    if type(val_a) == int or type(val_a) == float:
+        if type(val_b) == float or type(val_b) == int:
+            return val_a - val_b
+        else:
+            raise Exception("Second value must be numeric.")
+    else:
+        raise Exception("First value must be numeric.")
+
+def multiply(val_a: int|float, val_b: int|float) -> int | float:
+    if type(val_a) == int or type(val_a) == float:
+        if type(val_b) == float or type(val_b) == int:
+            return val_a * val_b
+        else:
+            raise Exception("Second value must be numeric.")
+    else:
+        raise Exception("First value must be numeric.")
+
+def divide(val_a: int|float, val_b: int|float) -> int | float:
+    if type(val_a) == int or type(val_a) == float:
+        if type(val_b) == float or type(val_b) == int:
+            if val_b != 0:
+                return val_a / val_b
+            else:
+                raise ZeroDivisionError("You cannot divide by 0")
         else:
             raise Exception("Second value must be numeric.")
     else:
